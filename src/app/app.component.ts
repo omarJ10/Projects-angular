1526b5063ff3f398c358b5ba5bb47fa6b2aaf95f
unknown <jalledomar2001@gmail.com>
2022-10-09 12:01:18 +0100

    bookstore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'bookstore';
 
-  books = [
-    {id : 1,title :"the slight edge",author :"jeff oslon",prix :23.00},
-    {id : 2,title :"atomic habits",author :"james clear",prix :18.00},
-    {id : 3,title :"the slight edge",author :"call newport",prix :18.70}
-  ]
+  
 }
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  books = [
+    {id : 1,title :"the slight edge",author :"jeff oslon",prix :23.00},
+    {id : 2,title :"atomic habits",author :"james clear",prix :18.00},
+    {id : 3,title :"the slight edge",author :"call newport",prix :18.70}
+  ]
+}
